348d6683757bb5739aa3875e90c3dfe6f7a8d1b2
Eider Muñoz <eider.munoz@asmetsalud.com>
2024-01-28 16:50:59 -0500

    Implementacion de repositorios

---
+++ src/main/java/org/example/cliente_service/cliente/infrastructure/db/repositoryImpl/ClienteEntRepository.java
@@ -0,0 +1,23 @@
+package org.example.cliente_service.cliente.infrastructure.db.repositoryImpl;
+
+import org.example.cliente_service.cliente.domain.repository.ClienteRepo;
+import org.example.cliente_service.cliente.infrastructure.db.entity.ClienteEnt;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ClienteEntRepository extends ClienteRepo<ClienteEnt, Integer> {
+    @Override
+    @Query(value = "SELECT * FROM cliente c inner join persona p on p.identificacion=c.identificacion", nativeQuery = true)
+    public List<ClienteEnt> buscarTodos();
+    @Override
+    @Modifying
+    @Query(value = "DELETE FROM ClienteEnt l WHERE l.identificacion = ?1")
+    public void eliminarPorIdentificacion(int identificacion);
+    @Override
+    @Query(value = "SELECT l FROM ClienteEnt l WHERE l.identificacion = ?1")
+    public ClienteEnt buscarPorIdentificacion(int identificacion);
+}
